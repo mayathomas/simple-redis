b57f76acc5a86e79d1f47c4c51748e55dfde4403
maya <770257551@qq.com>
2024-07-25 15:28:28 +0800

    hmap getall test

--- src/cmd/hmap.rs
+++ src/cmd/hmap.rs
@@ -149,4 +149,45 @@ mod tests {
 
         Ok(())
     }
+
+    #[test]
+    fn test_hset_hget_hgetall_commands() -> Result<()> {
+        let backend = crate::Backend::new();
+        let cmd = HSet {
+            key: "map".to_string(),
+            field: "hello".to_string(),
+            value: RespFrame::BulkString(b"world".into()),
+        };
+        let result = cmd.execute(&backend);
+        assert_eq!(result, RESP_OK.clone());
+
+        let cmd = HSet {
+            key: "map".to_string(),
+            field: "hello1".to_string(),
+            value: RespFrame::BulkString(b"world1".into()),
+        };
+        cmd.execute(&backend);
+
+        let cmd = HGet {
+            key: "map".to_string(),
+            field: "hello".to_string(),
+        };
+        let result = cmd.execute(&backend);
+        assert_eq!(result, RespFrame::BulkString(b"world".into()));
+
+        let cmd = HGetAll {
+            key: "map".to_string(),
+            sort: true,
+        };
+        let result = cmd.execute(&backend);
+
+        let expected = RespArray::new([
+            BulkString::from("hello").into(),
+            BulkString::from("world").into(),
+            BulkString::from("hello1").into(),
+            BulkString::from("world1").into(),
+        ]);
+        assert_eq!(result, expected.into());
+        Ok(())
+    }
 }
